20267f1fbf722531c8a60a6927402b5f6f1da2d2
tnd251197 <widter1234@gmail.com>
2025-05-15 02:02:04 +0700

    baitap2ss11.js

---
+++ baitap2ss11.js
@@ -0,0 +1,8 @@
+// cho người dùng nhập câu trả lời.
+let answer = prompt("What is the “official” name of JavaScript?");
+// đặt điều kiện và hiển thị ra màn hình.
+    if(answer === `ECMAScript` ){
+        alert("Right");
+    }else{
+        alert("Didn’t know? ECMAScript!");
+    }
