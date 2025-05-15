b2c39706e33cc95c2a338727a7a8d0c1228923f3
tnd251197 <widter1234@gmail.com>
2025-05-15 21:40:24 +0700

    baitap1.2ss12.js

---
+++ baitap1.2ss12.js
@@ -0,0 +1,7 @@
+// khai báo số tuổi 
+let age= +prompt("Xin hãy nhập vào tuổi của bạn ");
+// xét điều kiện
+if (age < 15){
+    alert("Học sinh không điều kiện vào lớp 10");
+}else 
+alert("Học sinh đủ điều kiện vào lớp 10");
