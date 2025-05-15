f80482ca27b114f6e0ce37f306a760efd423c438
tnd251197 <widter1234@gmail.com>
2025-05-15 09:14:52 +0700

    baitap3ss11.js

---
+++ baitap3ss11.js
@@ -0,0 +1,36 @@
+// cho phép người dùng nhập số từ 0 đến 9
+let number= +prompt("nhập một số từ 0 đến 9 ");
+switch(number){
+    case 0:
+        alert("số không");
+        break;
+        case 1:
+        alert("số một");
+        break;
+        case 2:
+        alert("số hai");
+        break;
+        case 3:
+        alert("số ba");
+        break;
+        case 4:
+        alert("số bốn");
+        break;
+        case 5:
+        alert("số năm");
+        break;
+        case 6:
+        alert("số sáu");
+        break;
+        case 7:
+       alert("số bảy");
+        break;
+        case 8:
+       alert("số tám");
+        break;
+        case 9:
+        alert("số chín");
+        break;
+
+        default: alert("số không có trong danh sách")
+}
