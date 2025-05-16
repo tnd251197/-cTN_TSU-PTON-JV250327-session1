3ccfd230cda7d8fb1f3a5d767eae38b24d7d4190
tnd251197 <widter1234@gmail.com>
2025-05-17 05:47:41 +0700

    baitap2.5ss12.js

---
+++ baitap2.5ss12.js
@@ -0,0 +1,5 @@
+// tính diện tích tam giác vuông biết 2 cạnh a và b 
+let a = +prompt("Nhập vào chiều dài một cạnh: ");
+let b = +prompt("Nhập vào chiều dài một cạnh: ");
+let S = ( a + b )/2;
+alert("Diện tích tam giác vuông:" + S );
