85bae057b34d40d36dd7c31ac690dc4bb840edd2
tnd251197 <widter1234@gmail.com>
2025-05-16 21:58:14 +0700

    baitap2.4ss12.js

---
+++ baitap2.4ss12.js
@@ -0,0 +1,5 @@
+// tính diện tích hình chữ nhật
+let a = +prompt("Nhập vào cạnh của hình chữ nhật:");
+let b = +prompt ("Nhập vào cạch của hình chữ nhật");
+let S = a * b;
+alert("Diện tích hình chữ nhật:" + S);
