947ec8dfad343543b3f5f7ea85f0a84b3ec932d8
tnd251197 <widter1234@gmail.com>
2025-05-16 20:12:27 +0700

    baitap1.4ss12.js

---
+++ baitap1.4ss12.js
@@ -0,0 +1,5 @@
+let a= +prompt("Nhập số nguyên a");
+let b= +prompt("Nhập số nguyên b");
+let c= +prompt("nhập số nguyên c");
+let maxNumber= Math.max(a,b,c);
+alert("Gía trị lớn nhất là:"+ maxNumber);
