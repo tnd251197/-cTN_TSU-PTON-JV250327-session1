8732d7c80da69be85bf690f95d4ccc667b3bf200
tnd251197 <widter1234@gmail.com>
2025-05-16 11:05:02 +0700

    baitap1.3ss12.js

---
+++ baitap1.3ss12.js
@@ -0,0 +1,9 @@
+let a= +prompt("Nhập một số nguyên bất kỳ");
+if ( a < 0 ) {
+    alert("Số nguyên a nhỏ hơn 0");
+}else if ( a > 0 ){
+    alert("Số nguyên a lớn hơn 0");
+
+}else{
+    alert("số bạn nhập bằng 0");
+}
