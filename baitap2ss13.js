f7b187ab8f541d4561440a098da6427acb46a9b7
tnd251197 <widter1234@gmail.com>
2025-05-21 04:59:06 +0700

    baitap2ss13.js

---
+++ baitap2ss13.js
@@ -0,0 +1,8 @@
+let number = [ 1, 5, 9, 10, 30, 52, 100];
+// cho người dùng nhập số bất kì
+let nhapSo = +prompt ("Mời người dùng nhập số ngẫu nhiên:");
+// điều kiện
+if (number.includes(nhapSo)){
+    alert("Bingo");
+}  else 
+alert("Chúc bạn mai mắn lần sau")
