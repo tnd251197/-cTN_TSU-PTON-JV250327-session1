b9d400842c1d51360d694866baf40354706ccdc2
tnd251197 <widter1234@gmail.com>
2025-06-09 09:28:16 +0700

    baitap2ss17.js

---
+++ baitap2ss17.js
@@ -0,0 +1,5 @@
+const toggle = document.getElementById(`toggle`);
+toggle.addEventListener('click', function (){
+document.body.classList.toggle ('dark-mode');
+document.body.classList.toggle ('light-mode ')
+});
